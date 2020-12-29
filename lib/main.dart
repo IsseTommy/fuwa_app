245c56a86cf41ed292b6429c15d0f430b417be8d
IsseTommy <1999yuto@gmail.com>
2020-12-29 09:57:18 +0900

    Change Icon

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FuwaSound',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
       home: MyHomePage(),
